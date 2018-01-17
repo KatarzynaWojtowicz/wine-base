d79fbda1e4928fac0b3c55553625cd8c8ff080c3
Katarzyna Wojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-01-17 22:29:30 +0100

    Refactor

--- src/main/java/pl/katarzynawojtowicz/winebase/ComboBoxes.java
+++ src/main/java/pl/katarzynawojtowicz/winebase/ComboBoxes.java
@@ -3,21 +3,21 @@ package pl.katarzynawojtowicz.winebase;
 public class ComboBoxes {
 	private static final String[] colourWine = { "pink", "red", "white", "other" };
 
-	private static final String[] crapeVariety = { "chardonnay", "sauvignon blanc", "riesling", "cabernet sauvignon",
+	private static final String[] CRAPE_VARIETY = { "chardonnay", "sauvignon blanc", "riesling", "cabernet sauvignon",
 			"merlot", "pinot noir", "syrah" };
 
-	private final static String[] type = { "dry", "semi - dry", "sweet", "semi - sweet" };
+	private final static String[] TYPE = { "dry", "semi - dry", "sweet", "semi - sweet" };
 
 	public static String[] getColourwine() {
 		return colourWine;
 	}
 
 	public static String[] getCrapevariety() {
-		return crapeVariety;
+		return CRAPE_VARIETY;
 	}
 
 	public static String[] getType() {
-		return type;
+		return TYPE;
 	}
 
 }
